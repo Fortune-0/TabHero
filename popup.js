e9033a3518f2ff0a8a1dcd39b8e4cc5912b9c476
fortune-0 <fortunepeterspc07@gmail.com>
2024-10-08 05:20:54 +0100

    basic front-end

--- popup.js
+++ popup.js
@@ -1,8 +1,30 @@
-document.getElementById("changeColor").addEventListener("click", () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) =>  {
-        chrome.scripting.executeScript({
-            target: { tabId: tabs[0].id},
-            func: () => {document.body.style.backgroundColor = "green"; }
-        });
-    });
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const searchInput = document.getElementById('searchInput');
+    const tabList = document.getElementById('tabList');
+
+    function displayTabs(tabs) {
+         tabList.innerHTML = '';
+                 tabs.forEach(tab => {
+                     const tabItem = document.createElement('li');
+                     tabItem.textContent = tab.title;
+                     tabItem.addEventListener('click', () => {
+                                              chrome.tabs.update(tab.id, { active: true });
+                                          });
+                                          tabList.appendChild(tabItem);
+                 })
+    }
+
+    function searchTabs() {
+         const query = searchInput.value.toLowerCase();
+         chrome.tabs.query({}, (tabs) => {
+            const filteredTabs = tabs.filter((tab) => 
+              tab.title.toLowerCase().includes(query) || tab.url.toLowerCase().includes(query)
+            );
+            displayTabs(filteredTabs);
+          });
+    }
+
+    chrome.tabs.query({}, displayTabs);
+
+    searchInput.addEventListener('input', debounce(searchTabs, 70));
+})   
